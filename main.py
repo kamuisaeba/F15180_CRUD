d10545f5d5ff3afbaf64ea18de922fc9e89b300f
kamuisaeba <kamuisaeba@gmail.com>
2016-06-05 14:13:17 +0200

    Rescatando datos de combo y treeView desde base de datos

--- main.py
+++ main.py
@@ -2,6 +2,8 @@
 # -*- coding: utf-8 -*-
 
 from gi.repository import Gtk
+import MySQLdb
+
 
 class Handler:
     def __init__(self):
@@ -29,9 +31,26 @@ class Handler:
         self.about   = builder.get_object("aboutdialog")
         self.form    = builder.get_object("newEditForm")
         self.delform = builder.get_object("DeleteDialog")
+
+        #Generamos el item del combo
+        query= "SELECT pkUser FROM user;" 
+        micursor = Conexion.cursor()   
+        micursor.execute(query);
+        bdData = micursor.fetchall()
+        micursor.close()
+        self.comboboxFila = builder.get_object("comboboxFila")
+        set_combo_from_list(self.comboboxFila,bdData)
+
+        #Generamos el iterador para el modelo TreeView
+        self.treeview  = builder.get_object('VistaTabla')
+        self.store = builder.get_object('Datos')
+        refresh(self)
+
+        #Mostramos la ventana inicial
         self.window.show_all()
         self.window.resize(300,300)
 
+    #definición de las señales
     def onDeleteWindow(self, *args):
         print 'close'
         Gtk.main_quit(*args)
@@ -75,9 +94,45 @@ class Handler:
 def main():
     window = Handler()
     Gtk.main()
-    print 'exit'
+    micursor.close()
     return 0
 
+
+def set_combo_from_list (cb, items):
+    """Setup a ComboBox or ComboBoxEntry based on a list of strings."""           
+    model = Gtk.ListStore(int)
+    for i in items:
+        print i
+        model.append(i)
+    cb.set_model(model)
+    cell = Gtk.CellRendererText()
+    cb.pack_start(cell, True)
+    cb.add_attribute(cell, 'text', 0)
+
+def create_user():
+    return 0
+def refresh(cb,*args):
+    query = "select nombre,apellidos,edad,color,sexo from user"
+    micursor = Conexion.cursor(MySQLdb.cursors.DictCursor)  
+    micursor.execute(query);
+    bdData = micursor.fetchall()
+    for currentData in bdData:
+        print currentData
+        cb.store.append([currentData['nombre'],
+            currentData['apellidos'],
+            currentData['edad'],
+            currentData['color'],
+            currentData['sexo']
+            ])
+    for i, column_title in enumerate(["Nombre","Apellidos","Edad","Color","Sexo"]):
+            renderer = Gtk.CellRendererText()
+            column = Gtk.TreeViewColumn(column_title, renderer, text=i)
+            cb.treeview.append_column(column)
+
+
+
+
 if __name__ == '__main__':
+    Conexion = MySQLdb.connect(host='localhost', user='acastillo', passwd ='acastillo', db='crud')  
     main()
 
