85c88b81bb0ea1a1fd636002dafeb665ee905487
kamuisaeba <kamuisaeba@gmail.com>
2016-06-06 23:22:36 +0200

    limpiando y ordenando funciones. Refinando proceso de eliminacion

--- main.py
+++ main.py
@@ -13,8 +13,6 @@ class Handler:
 
         self.selectedRow = None
 
-
-
         self.handlers = {
             "onDeleteWindow": self.onDeleteWindow,
             "onOpenAbout": self.onOpenAbout,
@@ -28,7 +26,8 @@ class Handler:
             "onEditUser" : self.onEditUser,
             "onNewUser" : self.onNewUser,
             "onCloseMain" : self.onDeleteWindow,
-            "onSelectTableRow" : self.onSelectTableRow
+            "onSelectTableRow" : self.onSelectTableRow,
+            "onCloseResponse" : self.onCloseResponse
         }
         self.builder.connect_signals(self.handlers)
 
@@ -40,24 +39,44 @@ class Handler:
         self.errorMessage = self.builder.get_object("ErrorMessage")
 
         #Generamos el item del combo
-        query= "SELECT pkUser FROM user;" 
-        micursor = Conexion.cursor()   
-        micursor.execute(query);
-        bdData = micursor.fetchall()
-        Conexion.commit()
-        micursor.close()
-        self.comboboxFila = self.builder.get_object("comboboxFila")
-        set_combo_from_list(self.comboboxFila,bdData)
+        self.init_combo()
 
         #Generamos el iterador para el modelo TreeView
         self.treeview  = self.builder.get_object('VistaTabla')
         self.store = self.builder.get_object('Datos')
-        initColumns(self)
+        self.initColumns()
         refresh(self)
 
         #Mostramos la ventana inicial
         self.window.show_all()
         self.window.resize(300,300)
+
+    #Inicia las columnas del treeView
+    def initColumns(cb):
+        for index,col in enumerate (cb.treeview.get_columns()):
+            print col
+            print index
+            cell = Gtk.CellRendererText()
+            col.pack_start (cell,True)
+            # Attributes for the column - make it display text of column 0
+            col.add_attribute (cell, "text", index)    
+    #Inicia el combo
+    def init_combo (self):
+        """Setup a ComboBox or ComboBoxEntry based on a list of strings."""  
+        query= "SELECT pkUser FROM user;" 
+        micursor = Conexion.cursor()   
+        micursor.execute(query);
+        items = micursor.fetchall()
+        Conexion.commit()
+        micursor.close()
+        cb = self.comboboxFila = self.builder.get_object("comboboxFila")         
+        model = Gtk.ListStore(int)
+        for i in items:
+            model.append(i)
+        cb.set_model(model)
+        cell = Gtk.CellRendererText()
+        cb.pack_start(cell, True)
+        cb.add_attribute(cell, 'text', 0)
 
     #definición de las señales
     def onDeleteWindow(self, *args):
@@ -66,9 +85,10 @@ class Handler:
     #EVENTOS VENTANA FORMULARIO
     def onSaveForm(self,*args):
         print 'save form'
-        create_user(self,*args)
-        refresh(self)
-        self.form.hide()
+        if validate_form(self):
+            create_user(self,*args)
+            refresh(self)
+            self.form.hide()
 
     def onCancelForm(self,*args):
         self.form.hide()
@@ -78,6 +98,7 @@ class Handler:
     def onDelconfirm(self,*args):
         print 'del confirm'
         if not self.selectedRow:
+            self.delform.hide()
             self.errorMessage.show()
             return 0
         query = "delete from user where pkUser = "+ str(self.selectedRow)
@@ -104,11 +125,13 @@ class Handler:
         self.delform.show()
 
     def onEditUser(self,*args):
-
+        clear_form(self)
+        self.builder.get_object("filaSeleccionada").set_text(str(self.selectedRow))
         self.form.show()
         print 'edit_user'  
 
     def onNewUser(self,*args):
+        clear_form(self)
         self.form.show()
         print 'new_user'
 
@@ -130,21 +153,26 @@ class Handler:
             print("Selected: ID=%d" % (row_id))
             self.selectedRow = row_id
 
+    def onCloseResponse(self, widget, data=None):
+        widget.hide()
 
 
 
 def main():
     window = Handler()
     Gtk.main()
-    micursor.close()
     return 0
 
 
+#Elimina un item del combo
 def delete_from_combo(cb,combo):
     print 'delete from combo'
     tree_iter = combo.get_active_iter()
     model = combo.get_model()
     model.remove(tree_iter)
+
+
+#añade item al combo
 def add_to_combo(cb,combo,id):
     print 'add to combo'
     print id
@@ -152,16 +180,23 @@ def add_to_combo(cb,combo,id):
     print model
     model.append([id])
 
-def set_combo_from_list (cb, items):
-    """Setup a ComboBox or ComboBoxEntry based on a list of strings."""           
-    model = Gtk.ListStore(int)
-    for i in items:
-        model.append(i)
-    cb.set_model(model)
-    cell = Gtk.CellRendererText()
-    cb.pack_start(cell, True)
-    cb.add_attribute(cell, 'text', 0)
 
+#refresca los elementos del combo
+def refresh_combo(combo):
+    model = combo.get_model()
+    model.clear()
+    query = "SELECT pkUser FROM user;" 
+    micursor = Conexion.cursor()
+    micursor.execute(query)
+    bdData = micursor.fetchall()
+    Conexion.commit()
+    micursor.close()
+    for i in bdData:
+        model.append(i)
+
+
+
+#Crea un usuario desde el formulario
 def create_user(self,*args):
     nombre = self.builder.get_object("form_nombre").get_text()
     apellidos = self.builder.get_object("form_apellidos").get_text()
@@ -185,27 +220,20 @@ def create_user(self,*args):
     micursor.close()
 
 
-def initColumns(cb,*args):
-    for index,col in enumerate (cb.treeview.get_columns()):
-        print col
-        print index
-        cell = Gtk.CellRendererText()
-        col.pack_start (cell,True)
-        # Attributes for the column - make it display text of column 0
-        # from the model
-        col.add_attribute (cell, "text", index)    
 
-
+#Refresca los datos (combo y tabla)
 def refresh(cb,*args):
     print 'refreshing'
-    query = "select nombre,apellidos,edad,color,sexo from user"
+    query = "select pkUser,nombre,apellidos,edad,color,sexo from user"
     micursor = Conexion.cursor(MySQLdb.cursors.DictCursor)  
     micursor.execute(query);
     bdData = micursor.fetchall()
     cb.store.clear()
     for currentData in bdData:
         print currentData
-        cb.store.append([currentData['nombre'],
+        cb.store.append([
+            currentData['pkUser'],
+            currentData['nombre'],
             currentData['apellidos'],
             currentData['edad'],
             currentData['color'],
@@ -213,6 +241,21 @@ def refresh(cb,*args):
             ])
     micursor.close()
     Conexion.commit()
+    refresh_combo(cb.builder.get_object("comboboxFila"))
+
+
+#limpia el formulario
+def clear_form(window):
+    print 'clear'
+    window.builder.get_object("form_nombre").set_text('')
+    window.builder.get_object("form_apellidos").set_text('')
+    window.builder.get_object("form_edad").set_text('')
+
+
+#Valida los datos del formulario
+def validate_form(cb):
+    print 'validate'
+    return None
 
 
 
